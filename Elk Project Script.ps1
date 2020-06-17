afa0239f37c53398ace22f70e77a640c8c0cc2ae
Cullen Sutherland <cullen.sutherland@gmail.com>
2020-06-17 14:24:20 -0500

    ReadMe Update

--- Elk Project Script.ps1
+++ Elk Project Script.ps1
@@ -1,3 +1,27 @@
 ## Connecting to Azure
 Connect-AzAccount
 Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
+
+############################
+### Define These Variables
+############################
+$resourceGroupName = 'ElkResourceGroup'
+$location = 'EastUS'
+$vm1 = 'ELK1'
+$lbname = "LoadBalancer"
+$protocol = 'TCP'
+$healthcheckprotocol = 'http'
+$insideport = 80
+$outsideport = 80
+$healthcheckpath = "/"
+$adminuser  = 'AzureAdmin'
+
+# Hotfix for mac
+$profile = $null
+$profile = $env:USERPROFILE
+if (!($profile)) { $profile = $home }
+# Create an SSH Key in azure acceptable format
+if (!(test-path "$profile\.ssh\id_rsa.pub")){
+  ssh-keygen -C $me -t rsa -b 4096 -f $profile\.ssh\id_rsa -N '""'
+}
